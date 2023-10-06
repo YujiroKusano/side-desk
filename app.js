f295fa8b0e314d44c9384978fc8480bc1cf87c18
YujiroKusano <yujiro.kusano.1106@gmail.com>
2023-10-06 16:02:19 +0900

    [Add] express,json

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (request, response) => {
-  response.status(200).send("Hello World");
+app.use(express.json());
+app.use(express.urlencoded( { extended: true }));
+
+app.get("/name/:name", (req, res) => {
+  res.status(200).send("Hello" + " " + req.params.name);
+});
+
+app.post("/name", (req, res) => {
+  console.log(req.body);
+  res.status(200).send("Hello" + " " + req.body.name);
 });
 
 app.listen(PORT, () => {
