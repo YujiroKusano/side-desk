a024348e1387c49add1aa1c71b2aa74e6694afbb
YujiroKusano <yujiro.kusano.1106@gmail.com>
2023-10-06 16:08:53 +0900

    [Add] Comment

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.use(express.json());
 app.use(express.urlencoded( { extended: true }));
 
 app.get("/name/:name", (req, res) => {
+  console.log(req.params.name);
   res.status(200).send("Hello" + " " + req.params.name);
 });
 
+// curl -X POST -H 'Content-Type: application/json;charset=UTF-8' -d '{ "name": "aaaaaa" }' http://localhost:3000/name/
 app.post("/name", (req, res) => {
   console.log(req.body);
   res.status(200).send("Hello" + " " + req.body.name);
